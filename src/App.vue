60fce0d91fa9568953020630f89886c8fd39e2d3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-27 14:00:53 +0100

    Passed the FooterComponent test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <h1>Rock Paper Scissors</h1>
   <HomePage msg="I am creating a new Vue game" />
+  <FooterComponent />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HomePage from './components/HomePage.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
 
 export default defineComponent({
   name: 'App',
   components: {
-    HomePage
+    HomePage,
+    FooterComponent
   }
 });
 </script>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>I am a footer</h3>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
