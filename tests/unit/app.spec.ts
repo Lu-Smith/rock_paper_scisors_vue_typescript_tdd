daf68390b65cb18637ed4ac53d145a1efe166c2d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-27 13:57:45 +0100

    Updated testing for App.vue

--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -1,6 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import AppVue from "@/App.vue";
 import HomePage from "@/components/HomePage.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 
 describe('App.vue', () => {
     it('renders correctly all components', () => {
@@ -12,8 +13,12 @@ describe('App.vue', () => {
 
     it('renders all components', () => {
         const wrapper = shallowMount(AppVue)
+        //HomePage component
         const homeElement = wrapper.findComponent(HomePage)
         expect(homeElement.exists()).toBe(true)
+        //FooterComponent
+        const footerElement = wrapper.findComponent(FooterComponent)
+        expect(footerElement.exists()).toBe(true)
         
     })
 })
