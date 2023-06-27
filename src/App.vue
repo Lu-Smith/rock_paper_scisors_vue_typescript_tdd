f5eed750551cdd72693d089c18fb32a061fc2860
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-27 11:31:23 +0100

    Added scss

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hello world</h1>
+  <HomePage />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HomePage from './components/HomePage.vue';
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HomePage
+  }
+});
+</script>
+
+
+<style lang="scss">
+#app {
+  background: black;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>I am a home page</h2>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
