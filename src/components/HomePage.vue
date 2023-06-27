e3938444503810a1d70be70cf13630680e232ca3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-27 18:08:07 +0100

    Passed HomePage tests

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,31 +1,47 @@
 <template>
-  <div v-if="!name" class="player">
-    <input type="text" v-model="playerName" placeholder="Enter your name..."/>
-    <button class="confirm" type="submit" @click="submitName">Confirm</button>
+  <div v-if="!displayGame">
+    <div v-if="!name" class="player">
+      <input type="text" v-model="playerName" placeholder="Enter your name..."/>
+      <button class="confirm" type="submit" @click="submitName">Confirm</button>
+    </div>
+    <div v-else class="welcome-message">
+      Welcome, {{ playerName }}!
+    </div>
+    <button class="start-game" @click="startGame">Start</button>
   </div>
-  <div v-else class="welcome-message">
-    Welcome, {{ playerName }}!
+  <div v-else>
+    <MainGame />
   </div>
-  <button class="start-game">Start</button>
+  
 </template>
 
 <script lang="ts">
 import { ref } from 'vue';
+import MainGame from "@/components/MainGame.vue";
 
 export default {
   name: "HomePage",
+  components: {MainGame},
   setup() {
     const playerName = ref('');
     const name = ref(false);
+    const displayGame = ref(false)
 
     const submitName = () => {
         name.value = true;
     }
 
+    const startGame = () => {
+      alert('Hello')
+      displayGame.value = true
+    }
+
     return {
       playerName,
       name,
-      submitName
+      displayGame,
+      submitName,
+      startGame
     }
   }
 }
---
+++ src/components/MainGame.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Main Game</h2>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
