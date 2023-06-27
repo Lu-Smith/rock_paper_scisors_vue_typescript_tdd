21fcd590b6ff669010c375b2413e07352144fabe
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-27 13:54:11 +0100

    Updating testing

--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -1,5 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import AppVue from "@/App.vue";
+import HomePage from "@/components/HomePage.vue";
 
 describe('App.vue', () => {
     it('renders correctly all components', () => {
@@ -7,5 +8,12 @@ describe('App.vue', () => {
         const titleElement = wrapper.find('h1')
         expect(titleElement.exists()).toBe(true)
         expect(titleElement.text()).toBe('Rock Paper Scissors')
+    })
+
+    it('renders all components', () => {
+        const wrapper = shallowMount(AppVue)
+        const homeElement = wrapper.findComponent(HomePage)
+        expect(homeElement.exists()).toBe(true)
+        
     })
 })
