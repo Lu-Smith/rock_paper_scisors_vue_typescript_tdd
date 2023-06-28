c961d24f772cc5070887292d67f6289b1762c713
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-28 16:35:37 +0100

    Pass first test from MainGame

--- src/components/MainGame.vue
+++ src/components/MainGame.vue
@@ -1,9 +1,19 @@
 <template>
+  <TimerComponent />
   <h2>Main Game</h2>
+  <TimeComponent />
 </template>
 
 <script lang="ts">
+import TimeComponent from './TimeComponent.vue'
+import TimerComponent from './TimerComponent.vue'
+
 export default {
+  name: "MainGame",
+  components: {
+    TimerComponent,
+    TimeComponent
+  }
 
 }
 </script>
