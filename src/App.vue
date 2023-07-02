f6ad2d55236f4000e1a45e144406d825e4b82b8b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-02 17:45:24 +0100

    Updated TimeComponent

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <h1>Rock Paper Scissors</h1>
   <HomePage />
   <FooterComponent />
+  <TimeComponent />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HomePage from './components/HomePage.vue';
+import TimeComponent from './components/TimeComponent.vue'
 import FooterComponent from './components/FooterComponent.vue';
 
 
@@ -16,7 +18,8 @@ export default defineComponent({
   name: 'App',
   components: {
     HomePage,
-    FooterComponent
+    FooterComponent,
+    TimeComponent
   }
 });
 </script>
--- src/components/TimeComponent.vue
+++ src/components/TimeComponent.vue
@@ -1,10 +1,34 @@
 <template>
-  <h3>Time</h3>
+  <h3>{{ currentTime }}</h3>
 </template>
 
-<script lang="ts">
-export default {
 
+<script lang="ts">
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: "TimeComponent",
+  setup() {
+    const currentTime = ref('');
+
+    const updatedTime = () => {
+      const currentDate = new Date();
+      const hours = currentDate.getHours();
+      const minutes = currentDate.getMinutes();
+      const seconds = currentDate.getSeconds();
+
+      currentTime.value = `${hours}:${minutes}:${seconds}`;
+    };
+
+    onMounted(() => {
+      updatedTime();
+      setInterval(updatedTime, 1000);
+    });
+
+    return {
+      currentTime
+    };
+  }
 }
 </script>
 
