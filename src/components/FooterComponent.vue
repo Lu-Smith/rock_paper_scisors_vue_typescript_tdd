42f81cba186d16741cc60bae083c1620b839d96b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-04 13:18:59 +0100

    Updated Footer styles

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <h4>This game was coded by <a class="profile-link" href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a> and is available as an open-source project on <a class="gitHub-link" href="https://github.com/Lu-Smith/rock_paper_scisors_vue_typescript_tdd" target="_blank">GitHub</a>.</h4>
+  <h4 class="footer">This game was coded by <a class="profile-link" href="https://www.lunasmithart.com/" target="_blank">Luna Smith</a> and is available as an open-source project on <a class="gitHub-link" href="https://github.com/Lu-Smith/rock_paper_scisors_vue_typescript_tdd" target="_blank">GitHub</a>.</h4>
 </template>
 
 <script lang="ts">
@@ -9,5 +9,17 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../assets/variables';
+@import '../assets/mixins';
+
+@keyframes colorAnimation {
+    0% { color: $background-color-dark }
+    50% { color: black}
+    100% { color: $background-color-dark }
+}
+
+.footer {
+  animation: colorAnimation 2s infinite;
+}
 
 </style>
