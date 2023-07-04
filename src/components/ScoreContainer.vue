7556845bdb9586bd62ae90698b17b7e9fe0a63e1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-04 11:43:43 +0100

    Updated styles in ScoreContainer

--- src/components/ScoreContainer.vue
+++ src/components/ScoreContainer.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="score-container">
     <h3 class="score">Score:</h3>
-    <h4 class="player-score">You: {{ playerScore }}</h4>
-    <h4 class="computer-score">Computer: {{ computerScore }}</h4>
-    <h4 class="total-score">You won: {{ Math.floor(playerScore / (playerScore + computerScore) * 100) }}% games.</h4>
+    <h4 class="player-score">You: <span>{{ playerScore }}</span></h4>
+    <h4 class="computer-score">Computer: <span>{{ computerScore }}</span></h4>
+    <h4 class="total-score" v-if="playerScore > 0">You won: <span>{{ Math.floor(playerScore / (playerScore + computerScore) * 100) }}%</span> games.</h4>
   </div>
 </template>
 
@@ -14,6 +14,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../assets/variables';
+
+.score-container{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  background-color: $background-color-basic;
+  position: fixed;
+  bottom: 110px;
+  right: 20px;
+  border-radius: $border-radius-basic;
+}
 
 </style>
